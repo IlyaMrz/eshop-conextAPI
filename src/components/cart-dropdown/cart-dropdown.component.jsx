8e7d48f8b816d2e5d9d5a4c1ad8223cb74ca6752
morozov ilya <knpkaa@gmail.com>
2021-04-17 18:27:59 +0300

    outside click hook

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { withRouter } from 'react-router-dom';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
-import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { selectCartHidden, selectCartItems } from '../../redux/cart/cart.selectors';
 import { toggleCartHidden } from '../../redux/cart/cart.actions.js';
 
 import './cart-dropdown.styles.scss';
+import useOutsideClick from '../outsideClickHandler/useOutsideClick';
 
-const CartDropdown = ({ cartItems, history, dispatch }) => (
-  <div className='cart-dropdown'>
+const CartDropdown = ({ cartItems, history, hidden, toggleCartHidden }) => {
+  const ref = useRef();
+  useOutsideClick(ref, () => {
+    !hidden && toggleCartHidden()
+  });
+return (
+  <div className='cart-dropdown' ref={ref}>
     <div className='cart-items'>
       {cartItems.length ? (
         cartItems.map(cartItem => (
@@ -24,16 +30,21 @@ const CartDropdown = ({ cartItems, history, dispatch }) => (
     <CustomButton
       onClick={() => {
         history.push('/checkout');
-        dispatch(toggleCartHidden());
+        toggleCartHidden();
       }}
     >
       GO TO CHECKOUT
     </CustomButton>
   </div>
-);
+)};
 
 const mapStateToProps = createStructuredSelector({
-  cartItems: selectCartItems
+  cartItems: selectCartItems,
+  hidden: selectCartHidden
 });
 
-export default withRouter(connect(mapStateToProps)(CartDropdown));
+const mapDispatchToProps = dispatch => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden())
+});
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(CartDropdown));
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import OutsideClickHandler from 'react-outside-click-handler';
 
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
@@ -13,37 +12,33 @@ import { selectCurrentUser } from '../../redux/user/user.selectors';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
-import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
-const Header = ({ currentUser, hidden, toggleCartHidden }) => (
-  <div className='header'>
-    <Link className='logo-container' to='/'>
-      <Logo className='logo' />
-    </Link>
-    <div className='options'>
-      <Link className='option' to='/shop'>
-        SHOP
+
+const Header = ({ currentUser, hidden }) => (
+    <div className='header'>
+      <Link className='logo-container' to='/'>
+        <Logo className='logo' />
       </Link>
-      <Link className='option' to='/shop'>
-        CONTACT
-      </Link>
-      {currentUser ? (
-        <div className='option' onClick={() => auth.signOut()}>
-          SIGN OUT
-        </div>
-      ) : (
-        <Link className='option' to='/signin'>
-          SIGN IN
+      <div className='options'>
+        <Link className='option' to='/shop'>
+          SHOP
         </Link>
-      )}
-      <CartIcon />
+        <Link className='option' to='/shop'>
+          CONTACT
+        </Link>
+        {currentUser ? (
+          <div className='option' onClick={() => auth.signOut()}>
+            SIGN OUT
+          </div>
+        ) : (
+          <Link className='option' to='/signin'>
+            SIGN IN
+          </Link>
+        )}
+        <CartIcon />
+      </div>
+      {!hidden && <CartDropdown />}
     </div>
-    {hidden && 
-    <OutsideClickHandler onOutsideClick={toggleCartHidden}>
-      <CartDropdown />
-    </OutsideClickHandler>
-    }
-  </div>
 );
 
 const mapStateToProps = createStructuredSelector({
@@ -51,8 +46,6 @@ const mapStateToProps = createStructuredSelector({
   hidden: selectCartHidden
 });
 
-const mapDispatchToProps = dispatch => ({
-  toggleCartHidden: () => dispatch(toggleCartHidden())
-});
 
-export default connect(mapStateToProps, mapDispatchToProps)(Header);
+
+export default connect(mapStateToProps)(Header);
